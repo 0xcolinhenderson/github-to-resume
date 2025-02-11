776c417134b128a048c8a56b5e53e98855af5536
Colin Henderson <153252937+0xcolinhenderson@users.noreply.github.com>
2025-02-11 05:22:16 +0000

    implemented vercel  speed tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   variable: "--font-inter",
@@ -15,10 +16,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} antialiased`}>
-        {children}
-      </body>
-    </html>
+    <Analytics>
+      <SpeedInsights>
+        <html lang="en">
+          <body className={`${inter.variable} antialiased`}>
+            {children}
+          </body>
+        </html>
+      </SpeedInsights>
+    </Analytics>
   );
 }
